cafb2f2a8a34649407e6ddb2c914d5c99f332c9d
Code Sandbox <hello@codesandbox.io>
2021-10-24 08:29:06 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b6534bb8f4e00ce80401d332bcf1b366.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63de96b87b585f9da767ea3f9c640c88",
+    "url": "https://merfankarami.github.io/csb-lvwss/index.html"
+  },
+  {
+    "revision": "e6455e96af2dcfae87dc",
+    "url": "https://merfankarami.github.io/csb-lvwss/static/css/main.c92067a2.chunk.css"
+  },
+  {
+    "revision": "31e288a14ab349851ac5",
+    "url": "https://merfankarami.github.io/csb-lvwss/static/js/2.96003981.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "https://merfankarami.github.io/csb-lvwss/static/js/2.96003981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6455e96af2dcfae87dc",
+    "url": "https://merfankarami.github.io/csb-lvwss/static/js/main.84d4530c.chunk.js"
+  },
+  {
+    "revision": "70d8f0829e07f68bf350",
+    "url": "https://merfankarami.github.io/csb-lvwss/static/js/runtime-main.b3578b3e.js"
+  }
+]);
